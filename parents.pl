76dbf27c6d9bff32b01c4163a6f09e414020f262
ivanoff <2@ivanoff.org.ua>
2013-08-07 21:29:36 +0300

    add comments

--- parents.pl
+++ parents.pl
@@ -4,25 +4,26 @@ use strict;
 use Storable;
 use WWW::Mechanize;
 
-$| = 1;
-my $dir = ( $0 =~ m%^(.*/)% )[0];
-my $store = $dir.'sites';
-my $sites = -f $store? retrieve( $store ) : {};
+$| = 1;                                 #forces a flush right away
+my $dir = ( $0 =~ m%^(.*/)% )[0];       #where we're located
+my $store = $dir.'sites';               #path to hash storable file with domains
+my $sites = -f $store? retrieve( $store ) : {}; #retrieve previous domain names
 my $mech = WWW::Mechanize->new();
 
+#we will read tcpdump's output on port 80 to find Host with domain name
 open (STDIN,"/usr/sbin/tcpdump 'port 80' -vvvs 1024 -l -A |");
 while (<STDIN>) {
     next unless /Host: (\S+)\s$/;
     my $_ = $1;
-    next if $sites->{white}{$_};
-    eval{ $mech->get( 'http://'.$_ ) };
+    next if $sites->{white}{$_} || $sites->{black}{$_}; #skip if we already found domain name before
+    eval{ $mech->get( 'http://'.$_ ) }; #enter to the website to find bad words
     my $c = $mech->content();
+    #if we found more than 15 bad words, then ban it
     if ( 15 < $c =~ s/p[o0]rn[o0]?|sex|anal|tits|harcore|cumshots|blowjob|lesbian|pusy|fucking|orgasm|pissing//ig ) {
         if( !$sites->{black}{$_} ) {
-            open my $f, '>>', '/etc/hosts';
+            open my $f, '>>', '/etc/hosts';     #write black domain name to /etc/hosts to ban
             print {$f} "\n127.0.0.1\t$_";
             close $f;
-            `/usr/bin/killall firefox`;
         }
         $sites->{black}{$_} = 1;
     } else {
