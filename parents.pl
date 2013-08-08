8409ee34ec048f041e646e7233431213642cf1f9
ivanoff <2@ivanoff.org.ua>
2013-08-08 07:49:07 +0300

    retrive domain names file each time

--- parents.pl
+++ parents.pl
@@ -7,7 +7,6 @@ use WWW::Mechanize;
 $| = 1;                                 #forces a flush right away
 my $dir = ( $0 =~ m%^(.*/)% )[0];       #where we're located
 my $store = $dir.'sites';               #path to hash storable file with domains
-my $sites = -f $store? retrieve( $store ) : {}; #retrieve previous domain names
 my $mech = WWW::Mechanize->new();
 
 #we will read tcpdump's output on port 80 to find Host with domain name
@@ -15,6 +14,7 @@ open (STDIN,"/usr/sbin/tcpdump 'port 80' -vvvs 1024 -l -A |");
 while (<STDIN>) {
     next unless /Host: (\S+)\s$/;
     my $_ = $1;
+    my $sites = -f $store? retrieve( $store ) : {}; #retrieve previous domain names
     next if $sites->{white}{$_} || $sites->{black}{$_}; #skip if we already found domain name before
     eval{ $mech->get( 'http://'.$_ ) }; #enter to the website to find bad words
     my $c = $mech->content();
