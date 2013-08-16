67c94025d09ef4338fadd6bc3fa59a3ef3a5e40c
ivanoff <2@ivanoff.org.ua>
2013-08-16 22:38:48 +0300

    change license to Apache v.2

--- parents.pl
+++ parents.pl
@@ -1,22 +1,27 @@
 #!/usr/bin/perl -w
 
+# Simpleness Parents - block adult web-sites on linux OS
+# more on http://parents.simpleness.org/
+# Copyright 2013 Dimitry Ivanov <2@ivanoff.org.ua>
+# Licensed under the Apache License, Version 2.0.
+
 use strict;
 use Storable;
 use LWP::Simple;
 use utf8;
 
 die "Error: Must run as root (sudo $0 ".(join ' ', @ARGV).")\n" if $>;
-die &help_message if !@ARGV || $ARGV[0] =~ /^-*h(elp)?$/;       #show help message
+die &help_message if !@ARGV || $ARGV[0] =~ /^-*h(elp)?$/;       # show help message
 
-$| = 1;                                 #forces a flush right away
+$| = 1;                                         # forces a flush right away
 my $pid = &get_pid;
-my $dir = ( $0 =~ m%^(.*/)% )[0];       #where we're located
-my $store = $dir.'sites';               #path to hash storable file with domains
-my $sites = -f $store? retrieve( $store ) : {};     #retrieve previous domain names
-my $mech = WWW::Mechanize->new();
+my $dir = ( $0 =~ m%^(.*/)% )[0];               # where we're located
+my $store = $dir.'sites';                       # path to hash storable file with domains
+my $sites = -f $store? retrieve( $store ) : {}; # retrieve previous domain names
 
 $_ = $ARGV[0];
 
+# List domains
 /^list$/ && do {
     die "there is no information about domains" unless $sites;
     foreach my $type ( keys %$sites ) {
@@ -29,16 +34,18 @@ $_ = $ARGV[0];
     }
 };
 
+# Add domains to the list
 /^add$/ && do {
     &need_bw( $ARGV[1] );
     &need_name( $ARGV[2] );
     $sites->{$ARGV[1]}{$ARGV[2]} = 1;
     store $sites, $store;
     hosts( 'add', $ARGV[2] );
-    $ARGV[1]=($ARGV[1] eq 'black')? 'white' : 'black';
+    $ARGV[1]=($ARGV[1] eq 'black')? 'white' : 'black';  # trick - delete domain from oposite side
     $_ = 'delete';
 };
 
+# Delete domains from the list
 /^delete$/ && do {
     &need_name( $ARGV[1] );
     delete $sites->{$_}{$ARGV[1]} foreach 'white', 'black';
@@ -46,40 +53,45 @@ $_ = $ARGV[0];
     hosts( 'delete', $ARGV[1] );
 };
 
+# Restart the programm
 /^restart$/ && do {
-    $_ = 'stop-';
+    $_ = 'stop-';       # trick - stop/start operations 
 };
 
+# Stop the programm
 /^stop(-)?$/ && do {
     `kill $pid` if $pid;
     $pid = &get_pid;
-    $_ = ($1)? 'start' : 'status';
+    $_ = ($1)? 'start' : 'status';      # start (in case of restart's trick)
 };
 
+# Start the programm
 /^start$/ && do {
     die "Already running!\n" if $pid;
     my $pid_fork = fork();
     if ($pid_fork) {
-        $_ = 'status';
+        $_ = 'status';          # lets show status after start
     } else {
-	$0 = $0.' started';
-	#we will read tcpdump's output on port 80 to find Host with domain name
+        # main parents programm
+	$0 = $0.' started';     # rename runned process for get_pid subroutine
+	# We will read tcpdump's output on port 80 to find Host with domain name
         open (STDIN,"/usr/sbin/tcpdump 'port 80' -vvvs 1024 -l -A -i any |");
         while (<STDIN>) {
-            next unless /Host: (\S+)\s$/;
+            next unless /Host: (\S+)\s$/;                   # We are looking for domainname after 'Host:'
             my $_ = $1;
             next if $sites->{white}{$_} || $sites->{black}{$_}; # skip if we already found domain name before
-            my $c = eval{ get 'http://'.$_ };                   # download the website's homepages/refreshes to find bad words
+
+            # download the website's homepages/refreshes to find bad words
+            my $c = eval{ get 'http://'.$_ };
             $c = eval{ get 'http://'.$_.'/'.$1 } while $c =~ /meta.*?http-equiv="Refresh".*?content=".*?URL=(.*?)"/i;
-            #if we found more than 15 bad words, then ban it
-            $sites = -f $store? retrieve( $store ) : {};     #retrieve previous domain names again after slow download
+
+            $sites = -f $store? retrieve( $store ) : {};    # retrieve previous domain names again after slow sites
             if ( 15 < $c =~ s/p[o0]rn[o0]?|sex|anal\b|tits|harcore|cumshots|blowjob|lesbian|pusy|fucking|orgasm|pissing|pussy|порно|секс//ig ) {
-                hosts('add', $_);
+                hosts('add', $_);                           # if we found more than 15 bad words, then ban site
                 $sites->{black}{$_} = 1;
-        	`killall firefox`;
-#        	`killall chrome`;
+        	`killall firefox`;                          # `killall opera`; # or whatever your are
             } else {
-                $sites->{white}{$_} = 1;
+                $sites->{white}{$_} = 1;                    # if not found, then store domainname as white site
             }
             store $sites, $store;
         }
@@ -87,11 +99,14 @@ $_ = $ARGV[0];
     }
 };
 
+# show staturs of the programm
 /^status$/ && do {
     $pid = &get_pid;
     die ( ($pid)? "Running [PID $pid]\n" : "Stopped\n" );
 };
 
+# Add or delete hostname from /etc/hosts
+# Parameters are command and domain name.
 sub hosts {
     my( $_, $name ) = @_;
     /^delete$/ && do {
@@ -111,18 +126,22 @@ sub hosts {
     };
 }
 
+# Returns PID of running parents.pl process
 sub get_pid {
     `ps aux | grep parents.pl | grep started | grep -v grep | awk '{printf "%d", \$2}'`;
 }
 
+# Dies if parameter is not black or white
 sub need_bw {
     die "list type is not acceptable, only black or white. '$0 -h' for more info" if $_[0] !~ /^black|white$/;
 }
 
+# Dies unless parameter
 sub need_name {
     die "need domain name. '$0 -h' for more info" unless $_[0];
 }
 
+# Help message
 sub help_message {
 <<EOF;
 Block adult sites. 
