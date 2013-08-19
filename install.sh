43d4df46e8fbba2a457ff7e68512802ce43a2800
ivanoff <2@ivanoff.org.ua>
2013-08-19 23:02:20 +0300

    add comments and some features to install

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-name=/opt/parents/parents.pl
+echo installing Simpleness Parental Control
+echo more on http://parents.simpleness.org
 
-mkdir -p /opt/parents
+folder=/opt/parents     # main folder of the program on local computer
+name=parents            # name of the program on local computer
 
-curl https://raw.github.com/ivanoff/parents.simpleness.org/master/parents.pl >$name 2>/dev/null
+mkdir -p $folder        # create folder 
+chown root:root $folder # only root is owner and group owner
+chmod 0740 $folder      # nobody else can't enter to this folder
 
+# download last version from GitHub and make it runuble
+curl https://raw.github.com/ivanoff/parents.simpleness.org/master/parents.pl >$folder/$name 2>/dev/null
 chmod +x $name
 
-grep $name /etc/rc.local || echo /usr/bin/perl $name >> /etc/rc.local
+# run on start computer ( if not done yet )
+grep $folder/$name /etc/rc.local || echo /usr/bin/perl $folder/$name start >> /etc/rc.local
 
