4f32a397627758f7e8fc19a611e961021154c9eb
ivanoff <2@ivanoff.org.ua>
2013-08-31 23:30:13 +0300

    add links to useful information

--- packages/deb/parents-0.1-build.sh
+++ packages/deb/parents-0.1-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 equivs-build parents-0.1
+
+# https://wiki.debian.org/DebianRepository/HowTo/TrivialRepository
+# https://wiki.debian.org/Packaging/Files
+# http://unix.stackexchange.com/questions/78905/how-to-force-a-file-location-in-equivs-build
